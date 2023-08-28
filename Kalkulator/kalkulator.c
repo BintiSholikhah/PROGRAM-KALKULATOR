40b25d8e753efd153fa91ecf5d65bd90fe629779
BintiSholikhah <101707862+BintiSholikhah@users.noreply.github.com>
2023-08-28 10:28:34 +0700

    Program kalkulator tambah-kurang-kali-bagi

---
+++ Kalkulator/kalkulator.c
@@ -0,0 +1,18 @@
+#include "kalkulator.h"
+
+int tambah (int a, int b){
+	int hasil = a+b;
+	return hasil;
+}
+float kurang (float a, float b){
+	float hasil = a-b;
+	return hasil;
+}
+int kali (int a, int b){
+	int hasil = a*b;
+	return hasil;
+}
+float bagi (float a, float b){
+	float hasil = a/b;
+	return hasil;
+}
---
+++ Kalkulator/kalkulator.h
@@ -0,0 +1,7 @@
+#ifndef _KALKULATOR_H
+#define _KALKULATOR_H
+int tambah (int a, int b);
+float kurang (float a, float b);
+int kali (int a, int b);
+float bagi (float a, float b);
+#endif
